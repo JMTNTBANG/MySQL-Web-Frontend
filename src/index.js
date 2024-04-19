c2c1651f8e7bb83fb02eddc07bc367a9d2d57752
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-04-19 12:13:32 -0500

    Page Now Shows in HTML Table format and has Support for multiple rows

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
-var mysql = require('mysql');
-var http = require('http');
-var config = require('./config.json')
+var mysql = require("mysql");
+var http = require("http");
+var config = require("./config.json");
 
 var database = mysql.createConnection({
   host: config.server.ip,
   port: config.server.port,
   user: config.auth.username,
   password: config.auth.password,
-  database: config.database
+  database: config.database,
 });
 
-database.connect(function(err) {
-    if (err) throw err;
+database.connect(function (err) {
+  if (err) throw err;
 });
 
-http.createServer(function (req, page) {
-    database.query(`SELECT * FROM ${config.table}`, function (err, result, fields) {
+http
+  .createServer(function (req, page) {
+    database.query(
+      `SELECT * FROM ${config.table}`,
+      function (err, result, fields) {
         if (err) throw err;
-        var text = `Contents of ${config.table}: \n`
+        var text = `<style>table, th, td {border:1px solid black;}</style><h1>Contents of ${config.table}:</h1><table style="width:100%;"><tr>`;
         for (column of fields) {
-            text += `${column.name}: ${result[0][column.name]}\n`
+          text += `<th>${column.name}</th>`;
         }
+        text += `</tr>`;
+        for (row of result) {
+          text += `<tr>`;
+          for (value in row) {
+            text += `<td>${row[value]}</td>`;
+          }
+          text += `</tr>`;
+        }
+        text += `</table>`;
         page.write(text);
         page.end();
-    });
-}).listen(8080);
+      }
+    );
+  })
+  .listen(8080);
